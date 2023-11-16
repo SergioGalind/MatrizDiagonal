2ef104bc6e5ef4025e5f7bad0c6d7c0185ea505b
Serigo <s.erickgalindo3@gmail.com>
2023-11-15 23:41:18 -0600

    Configurando main

---
+++ src/Matriz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Matriz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 1. Ingresar los datos para el numero de filas y de columnas
+        System.out.println("Ingresa el numero de filas: ");
+        int filas = sc.nextInt();
+        int columnas = sc.nextInt();
+    }
+}
+
